fb1ab40b5926014a60084df360497ca0bf28ee1f
Sonawane-Shubham <sssonawane047@gmail.com>
2025-06-17 10:40:48 +0530

    change backend api url

--- api.js
+++ api.js
@@ -1,2 +1,3 @@
-export const BACKEND_API = 'http://209.182.232.11:4049/'
+export const BACKEND_API = 'https://api.lending-square.com/';
+//export const BACKEND_API = 'http://209.182.232.11:4049/';
 // export const BACKEND_API = "https://5c32-203-192-225-49.ngrok-free.app/"
